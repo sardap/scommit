7e1b1b6ea91a3581a44ceb215d4b586dce9d7a63
sardap <pfsarda23@gmail.com>
2020-08-07 18:37:36 +1000

    added downloading of dict file

--- spellchecker/spellchecker.go
+++ spellchecker/spellchecker.go
@@ -2,7 +2,12 @@ package spellchecker
 
 import (
 	"bufio"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"os/user"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
@@ -18,11 +23,59 @@ type Word struct {
 	Correct bool
 }
 
+const dictFileURL = "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt"
+
+func downloadDictFile(filePath string) {
+	// Create the file
+	out, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(dictFileURL)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Errorf("bad status: %s", resp.Status))
+	}
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func init() {
 	wordRe = regexp.MustCompile("^[A-Z]?[a-z]+$")
 	wordDict = make(map[string]bool)
 
-	file, err := os.Open("words_alpha.txt")
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	appDataPath := filepath.Join(usr.HomeDir, ".scommit")
+	dictFilePath := filepath.Join(appDataPath, "dict.txt")
+	if _, err := os.Stat(appDataPath); os.IsNotExist(err) {
+		fmt.Printf("First time Running!\nMaking program data folder at %s\n", appDataPath)
+		err := os.Mkdir(appDataPath, os.FileMode(0777))
+		if err != nil {
+			panic(err)
+		}
+	}
+	if _, err := os.Stat(dictFilePath); os.IsNotExist(err) {
+		fmt.Printf("Downloading dict file\n")
+		downloadDictFile(dictFilePath)
+		fmt.Printf("dict file downloaded\n")
+	}
+	file, err := os.Open(dictFilePath)
 	if err != nil {
 		panic(err)
 	}
